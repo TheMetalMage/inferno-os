54585e12a25582282e88045dd5a18ecb86fd8b31
Ramakrishnan Muthukrishnan <ram@rkrishnan.org>
2015-05-31 08:05:59 +0530

    mount.b: load the Dial module without which mount throws an error

---
+++ emu/Nt/r16.h
@@ -0,0 +1,10 @@
+typedef unsigned short Rune16;
+
+	wchar_t	*widen(char *s);
+	char		*narrowen(wchar_t *ws);
+	int		widebytes(wchar_t *ws);
+	int		runes16len(Rune16*);
+	Rune16*	runes16dup(Rune16*);
+	Rune16*	utftorunes16(Rune16*, char*, int);
+	char*	runes16toutf(char*, Rune16*, int);
+	int		runescmp(Rune16*, Rune16*);
